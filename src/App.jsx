b8082bf6525f6900aa6c7ee171f1fcc9175d7fca
rabbitonthemoon <lkpnthr@gmail.com>
2024-02-15 20:15:00 +0900

    edit chatroomlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,18 @@ import ChatRoomList from './components/ChatRoomList';
 import ChatRoom from './components/ChatRoom';
 import MainPage from './components/MainPage';
 import MapPage from "./components/MapPage";
-import ChatLayout from './components/ChatLayout';
 
 function App() {
   return (
     <ChatRoomsProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/map" element={<MapPage />} />
-              {/* <Route path="/chatroomlist" element={<ChatLayout><ChatRoomList /></ChatLayout>} /> */}
-              <Route path="/chatroom/:id" element={
-              <ChatLayout>
-                <ChatRoom />
-              </ChatLayout>
-              } />
-            </Routes>
-          </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/chatroomlist" element={<ChatRoomList />} /> {}
+          <Route path="/chatroom/:id" element={<ChatRoom />} />
+        </Routes>
+      </Router>
     </ChatRoomsProvider>
   );
 }
--- src/components/ChatRoomList.jsx
+++ src/components/ChatRoomList.jsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import ChatRoom from './ChatRoom';
 import io from 'socket.io-client';
+import { NavBar } from 'antd-mobile';
+import { LeftOutline } from 'antd-mobile-icons';
+import './ChatRoomList.css';
 
 let socket;
 
 function ChatRoomList() {
   const [rooms, setRooms] = useState([]);
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     // Socket 서버 연결
@@ -21,26 +25,36 @@ function ChatRoomList() {
     });
 
     return () => {
-      socket.disconnect();
+        socket.off('rooms');
+        socket.disconnect();
     };
   }, []);
 
   return (
-    <Router>
-      <div>
-        <h1>채팅방 목록</h1>
-        <ul>
-          {rooms.map((room, index) => (
-            <li key={index}>
-              <Link to={`/chat/${room.id}`}>{room.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* <Route path="/chat/:id" component={ChatRoom} /> */}
-      <Route path="/chatroom/:id" component={ChatRoom} />
-    </Router>
+    <div className="chat-room-container">
+      <NavBar
+        back={null}
+        icon={<LeftOutline />}
+        onBack={() => navigateTo('/')}
+        style={{
+          "--height": "50px",
+          "--border-bottom": "1px #eee solid",
+          "margin-bottom": "20px",
+        }}
+      >
+        채팅방 목록
+      </NavBar>
+      
+      <ul className="room-list">
+        {rooms.map((room, index) => (
+          <li key={index} className="room-item">
+            <Link to={`/chatroom/${room.id}`} className="room-link">
+              {room.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
