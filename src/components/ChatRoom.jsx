0fe7510db977ad79abf1e21b2148f436fab4c908
YoungWan Kwon <87213416+000wan@users.noreply.github.com>
2024-02-15 23:30:43 +0900

    Add match button

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,27 +1,38 @@
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import io from 'socket.io-client';
-import { useChatRooms } from './ChatRoomsProvider'; // Context 사용
-import './ChatRoom.css';
-import { NavBar } from 'antd-mobile';
-import { LeftOutline } from 'antd-mobile-icons';
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+import { useChatRooms } from "./ChatRoomsProvider"; // Context 사용
+import "./ChatRoom.css";
+import { NavBar, FloatingBubble, Popover } from "antd-mobile";
+import { LeftOutline, MoreOutline } from "antd-mobile-icons";
+import { SendOutlined, HeartOutlined, SmileOutlined } from "@ant-design/icons";
 
 let socket;
 
 //더미데이터
 const initialMessages = [
-  { id: 1, text: "안녕하세요^^", sender: "유성주민", timestamp: "2024-02-15T09:00:00Z" },
-  { id: 2, text: "하이ㅣㅣㅣ", sender: "유성주민", timestamp: "2024-02-15T09:01:00Z" },
+  {
+    id: 1,
+    text: "안녕하세요^^",
+    sender: "유성주민",
+    timestamp: "2024-02-15T09:00:00Z",
+  },
+  {
+    id: 2,
+    text: "하이ㅣㅣㅣ",
+    sender: "유성주민",
+    timestamp: "2024-02-15T09:01:00Z",
+  },
 ];
 
 function ChatRoom() {
   const { id } = useParams();
   const navigateTo = useNavigate();
   const { updateRoomsWithMessage } = useChatRooms(); // Context에서 함수 추출
-  const [currentMessage, setCurrentMessage] = useState('');
+  const [currentMessage, setCurrentMessage] = useState("");
   // const [chatMessages, setChatMessages] = useState([]);
-  const [messages, setMessages] = useState(initialMessages);// 더미데이터!!!!!!!
-  const [chatPartnerNickname, setChatPartnerNickname] = useState('유성주민');
+  const [messages, setMessages] = useState(initialMessages); // 더미데이터!!!!!!!
+  const [chatPartnerNickname, setChatPartnerNickname] = useState("유성주민");
 
   // 실제로는 이 코드
   // useEffect(() => {
@@ -47,13 +58,12 @@ function ChatRoom() {
         sender: "나",
         timestamp: new Date().toISOString(),
       };
-  
-      setMessages(messages => messages.concat(newMessage));
+
+      setMessages((messages) => messages.concat(newMessage));
       updateRoomsWithMessage(id, newMessage);
-      setCurrentMessage('');
+      setCurrentMessage("");
     }
   };
-  
 
   // 실제로는 이 코드
   // const sendMessage = () => {
@@ -61,7 +71,7 @@ function ChatRoom() {
   //     socket.emit('send message', currentMessage.trim());
   //     setCurrentMessage('');
   //   }
-  // };  
+  // };
 
   // return (
   //   <div>
@@ -77,6 +87,19 @@ function ChatRoom() {
   //     <button onClick={sendMessage}>Send</button>
   //   </div>
   // );
+
+  const actions = [{ key: "match", icon: <HeartOutlined />, text: "매치" }];
+
+  const right = (
+    <Popover.Menu
+      actions={actions}
+      placement="bottom-start"
+      onAction={(node) => console.log(`${node.text}`)}
+      trigger="click"
+    >
+      <MoreOutline fontSize={"20px"} />
+    </Popover.Menu>
+  );
 
   // 더미용
   return (
@@ -98,7 +121,8 @@ function ChatRoom() {
     <div className="chat-room-container">
       <NavBar
         icon={<LeftOutline />}
-        onBack={() => navigateTo('/chatroomlist')} 
+        onBack={() => navigateTo("/chatroomlist")}
+        right={right}
         style={{
           "--height": "50px",
           "--border-bottom": "1px #eee solid",
@@ -108,8 +132,13 @@ function ChatRoom() {
       </NavBar>
 
       <div className="messages-container">
-        {messages.map(message => (
-          <div key={message.id} className={`message-bubble ${message.sender === "나" ? "outgoing" : "incoming"}`}>
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className={`message-bubble ${
+              message.sender === "나" ? "outgoing" : "incoming"
+            }`}
+          >
             <p className="message-text">{message.text}</p>
             <p className="message-sender">{message.sender}</p>
           </div>
@@ -119,11 +148,17 @@ function ChatRoom() {
         <input
           className="message-input"
           value={currentMessage}
-          onChange={e => setCurrentMessage(e.target.value)}
+          onChange={(e) => setCurrentMessage(e.target.value)}
           type="text"
           placeholder="메시지 입력"
         />
-        <button className="send-button" onClick={sendMessage} disabled={!currentMessage.trim()}>Send</button>
+        <button
+          className="send-button"
+          onClick={sendMessage}
+          disabled={!currentMessage.trim()}
+        >
+          <SendOutlined />
+        </button>
       </div>
     </div>
   );
